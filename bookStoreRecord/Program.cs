10bccac9fa16f9e8e86c119abbcc3b8eb96cfe1d
Shane Reed <66423664+Timebinder12@users.noreply.github.com>
2023-03-01 10:25:17 -0500

    Add project files.

---
+++ bookStoreRecord/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public record Bookstore(int ID, string Title, string Author, double Price);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Bookstore book1 = new Bookstore(1, "Philosophers Stone", "J.K Rowling", 19.99);
+        Bookstore book2 = new Bookstore(2, "Chamber of Secrets", "J.K Rowling", 19.99);
+        Bookstore book3 = new Bookstore(3, "Prisoner of Azkaban", "J.K Rowling", 19.99);
+
+        Console.WriteLine(book1);
+        Console.WriteLine(book2);
+        Console.WriteLine(book3);
+    }
+}
